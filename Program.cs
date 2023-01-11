2dbd824506b4e95270a3394297a645a30926990c
Anna <happyz2104@gmail.com>
2023-01-12 00:19:10 +0300

    Создали поясняющий файл

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*System.Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if (number.Length==5)
+{
+if (number[0]==number[4]&&number[1]==number[3])
+{
+System.Console.WriteLine($"{number} является палиндромом");
+}
+else
+{
+    System.Console.WriteLine($"{number} не является палиндромом");
+}
+}
+
+else 
+{
+Console.WriteLine($"Вы ввели не пятизначное число");
+} */
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// (x1 - x2)^2 + (y1-y2)^2 + (z1-z2)^2)^(1/2)
+/*System.Console.Write("Введите коррдинату x точки А: ");
+int x1 = Convert.ToInt32(Console.ReadLine()); 
+System.Console.Write("Введите коррдинату y точки А: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коррдинату z точки А: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордианту x точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine()); 
+System.Console.Write("Введите коордианту y точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите коордианту z точки B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1-y2),2) + Math.Pow((z1-z2),2)),2); 
+System.Console.WriteLine($"Расстояние между двумя точками: {result}"); */
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); */
+/*for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine($"{Math.Pow(i, 3)} ");
+} */
+
+/*int count = 1;
+while(count <= num)
+{
+System.Console.Write(Math.Pow(count, 3)+" ");
+count++; 
+} */
+
+
